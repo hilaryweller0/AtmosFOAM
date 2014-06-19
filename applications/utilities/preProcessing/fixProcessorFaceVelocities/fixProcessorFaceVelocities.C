eeac617854c8693f7938b799344b14b6c4d2d97e
James Shaw <js102@zepler.net>
2014-06-19 14:14:41 +0100

    get fixProcessorFaceVelocities working on real case

--- applications/utilities/preProcessing/fixProcessorFaceVelocities/fixProcessorFaceVelocities.C
+++ applications/utilities/preProcessing/fixProcessorFaceVelocities/fixProcessorFaceVelocities.C
@@ -1,7 +1,47 @@
 #include "fvCFD.H"
+#include "GeometricField.H"
+#include "processorFvPatchField.H"
 
 int main(int argc, char *argv[])
 {
-    Info << "Hello world" << endl;
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    surfaceVectorField uf
+    (
+        Foam::IOobject
+        (
+            "Uf",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh
+    );
+
+    unsigned int c = 0;
+
+    forAll(mesh.boundaryMesh(), patchi)
+    {
+        if (mesh.boundaryMesh().types()[patchi] == word("processor"))
+        {
+            Field<vector>& boundaryField = uf.boundaryField()[patchi];
+            forAll(boundaryField, i)
+            {
+                if (boundaryField[i][0] < 0)
+                {
+                    boundaryField[i][0] = -boundaryField[i][0];
+                    c++;
+                }
+            }
+
+            Info << "Corrected " << c << " value(s) in patch " << mesh.boundaryMesh()[patchi].name() << endl;
+        }
+    }
+
+    uf.write();
+
     return 0;
 }
