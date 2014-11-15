c6e4a878ee630b2aa273ed402fcdb26da70219c9
James Shaw <js102@zepler.net>
2014-11-15 22:15:38 +0000

    get non-divergent advection test partially working; tracer is currently advected too quickly

--- advectionFoam.C
+++ advectionFoam.C
@@ -2,14 +2,103 @@
 #include "fvIOoptionList.H"
 #include "simpleControl.H"
 
+volScalarField readT(Time& runTime, fvMesh& mesh)
+{
+    Info<< "Reading field T\n" << endl;
+
+    return volScalarField
+    (
+        IOobject
+        (
+            "T",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh
+    );
+}
+
+volScalarField calculateDivUf(Time& runTime, fvMesh& mesh, surfaceScalarField& phi)
+{
+    Info<< "Calculating divergence of phi\n" << endl;
+
+    return volScalarField
+    (
+        IOobject(
+            "divUf",
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        fvc::div(phi)
+    );
+}
+
+surfaceScalarField readOrCalculatePhi(argList& args, Time& runTime, fvMesh& mesh)
+{
+    if (args.options().found("usePhi"))
+    {
+        return surfaceScalarField
+        (
+            IOobject
+            (
+                "phi",
+                runTime.timeName(),
+                mesh,
+                IOobject::MUST_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh
+        );
+    }
+    else
+    {
+        Info<< "Reading field Uf\n" << endl;
+
+        surfaceVectorField Uf
+        (
+            IOobject
+            (
+                "Uf",
+                runTime.timeName(),
+                mesh,
+                IOobject::MUST_READ,
+                IOobject::AUTO_WRITE
+            ),
+            mesh
+        );
+
+        Info<< "Calculating face flux field phi\n" << endl;
+
+        return surfaceScalarField
+        (
+            IOobject
+            (
+                "phi",
+                runTime.timeName(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::AUTO_WRITE
+            ),
+            Uf & mesh.Sf()
+        );
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    Foam::argList::addBoolOption("usePhi", "use 0/phi rather than calculating it from 0/Uf");
     #include "setRootCase.H"
     #include "createTime.H"
     #include "createMesh.H"
-    #include "createFields.H"
     #include "createFvOptions.H"
 
+    volScalarField T = readT(runTime, mesh);
+    surfaceScalarField phi = readOrCalculatePhi(args, runTime, mesh);
+    volScalarField divUf = calculateDivUf(runTime, mesh, phi);
     divUf.write();
 
     simpleControl simple(mesh);
