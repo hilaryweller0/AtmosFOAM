d950c0e14b1d89ca8b5776fbe95486ca4f036c78
James Shaw <js102@zepler.net>
2014-11-17 12:15:19 +0000

    define velocities at inlet and outlet boundaries

--- setHorizontalVelocityField.C
+++ setHorizontalVelocityField.C
@@ -1,5 +1,80 @@
 #include "fvCFD.H"
 
+class VelocityProfile
+{
+    public:
+    VelocityProfile(const IOdictionary& dict)
+        :
+            u0(readScalar(dict.lookup("maxVelocity"))),
+            z1(readScalar(dict.lookup("maxVelocityHeight"))),
+            z2(readScalar(dict.lookup("zeroVelocityHeight")))
+    {}
+
+    void applyTo(surfaceVectorField& field)
+    {
+        applyToInternalField(field);
+        applyToBoundary("inlet", field);
+        applyToBoundary("outlet", field);
+    }
+
+    private:
+    const scalar u0;
+    const scalar z1;
+    const scalar z2;
+    
+    void applyToInternalField(surfaceVectorField& field)
+    {
+        forAll(field, cellI)
+        {
+            const point& face = field.mesh().Cf()[cellI];
+            field[cellI] = velocityAt(face.z());
+        }
+    }
+
+    void applyToBoundary(const word name, surfaceVectorField& field)
+    {
+        label boundaryI = findBoundaryPatchIndex(field.mesh(), name);
+        forAll(field.boundaryField()[boundaryI], cellI)
+        {
+            const point& face = field.mesh().Cf().boundaryField()[boundaryI][cellI];
+            field.boundaryField()[boundaryI][cellI] = velocityAt(face.z());
+        }
+    }
+
+    vector velocityAt(const scalar z) const
+    {
+        if (z > z1 && z < z2)
+        {
+            return vector(u0*pow((Foam::sin(M_PI/2*(z-z1)/(z2-z1))),2), 0, 0);
+        }
+        else if (z >= z2)
+        {
+            return vector(u0, 0, 0);
+        }
+        else
+        {
+            return vector(0, 0, 0);
+        }
+    }
+
+    label findBoundaryPatchIndex(const fvMesh& mesh, const word& name)
+    {
+        forAll(mesh.boundaryMesh(), patchI)
+        {
+            if (mesh.boundaryMesh()[patchI].name() == name)
+            {
+                return patchI;
+            }
+        }
+        FatalErrorIn("setHorizontalVelocityField")
+            << " no boundary called " << name << ". The boundaries are called "
+            << mesh.boundaryMesh().names()
+            << exit(FatalError);
+
+        return -1;
+    }
+};
+
 int main(int argc, char *argv[])
 {
 #   include "setRootCase.H"
@@ -19,10 +94,6 @@ int main(int argc, char *argv[])
         )
     );
 
-    const scalar u0(readScalar(initDict.lookup("maxVelocity")));
-    const scalar z1(readScalar(initDict.lookup("maxVelocityHeight")));
-    const scalar z2(readScalar(initDict.lookup("zeroVelocityHeight")));
-
     surfaceVectorField Uf
     (
         IOobject("Uf", runTime.timeName(), mesh),
@@ -31,35 +102,9 @@ int main(int argc, char *argv[])
         "fixedValue"
     );
 
-    Info << "Creating velocity field U" << endl;
-    forAll(Uf, cellI)
-    {
-        const point& face = mesh.Cf()[cellI];
-
-        if (face.z() > z1 && face.z() < z2)
-        {
-            Uf[cellI] = vector(u0*pow((Foam::sin(M_PI/2*(face.z()-z1)/(z2-z1))),2), 0, 0);
-        }
-        else if (face.z() >= z2)
-        {
-            Uf[cellI] = vector(u0, 0, 0);
-        }
-    }
-
-    label inletBC = -1;
-    forAll(mesh.boundaryMesh(), patchI)
-    {
-        if (mesh.boundaryMesh()[patchI].name() == "inlet")
-        {
-            inletBC = patchI;
-        }
-    }
-    forAll(Uf.boundaryField()[inletBC], cellI)
-    {
-	Uf.boundaryField()[inletBC][cellI] = vector(0, 0, 0);
-    }
-
-    // TODO: define Uf at inlet and outlet boundaries
+    VelocityProfile velocityProfile(initDict);
+    Info << "Creating velocity field Uf" << endl;
+    velocityProfile.applyTo(Uf);
 
     Uf.write();
 }
