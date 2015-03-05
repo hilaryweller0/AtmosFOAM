486fad10e23a2a587edc868fa5142c57eed7c02a
James Shaw <js102@zepler.net>
2015-03-05 17:46:07 +0000

    add VelocityProfile::streamFunctionAt()

--- applications/utilities/preProcessing/setHorizontalVelocityField/HorizontalVelocityProfile.C
+++ applications/utilities/preProcessing/setHorizontalVelocityField/HorizontalVelocityProfile.C
@@ -26,3 +26,15 @@ vector HorizontalVelocityProfile::velocityAt(const point& p) const
         return vector(0, 0, 0);
     }
 }
+
+scalar HorizontalVelocityProfile::streamFunctionAt(const point& p) const
+{
+    const scalar z = p.z();
+
+    if (z <= z1) return 0;
+    else if (z <= z2)
+    {
+        return -0.5*u0*(z - z1 - (z2-z1)/M_PI*Foam::sin(M_PI*(z-z1)/(z2-z1)));
+    }
+    else return -0.5*u0*(2*z - z2 - z1);
+}
