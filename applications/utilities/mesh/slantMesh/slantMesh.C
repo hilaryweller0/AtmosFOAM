e625c6ca8e431107fa0528bdf23e09bb90b579e4
James Shaw <js102@zepler.net>
2015-11-23 13:09:18 +0000

    tools for generating slanting cell grids

---
+++ applications/utilities/mesh/slantMesh/slantMesh.C
@@ -0,0 +1,58 @@
+#include "fvCFD.H"
+#include "Mountain.H"
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createMesh.H"
+
+    IOdictionary dict
+    (
+        IOobject
+        (
+            "mountainDict",
+            mesh.time().constant(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+    
+    // New point locations layered over the mountain
+    IOField<point> newPoints
+    (
+        IOobject("points", mesh.time().constant(), "polyMesh", mesh),
+        mesh.points()
+    );
+
+    const word mountainType(dict.lookup("mountainType"));
+    Mountain* mountain;
+    if (mountainType == "SCHAER_COS")
+    {
+        mountain = new SchaerCosMountain(dict);
+    }
+    else if (mountainType == "SCHAER_EXP")
+    {
+        mountain = new SchaerExpMountain(dict);
+    }
+    else
+    {
+        FatalErrorIn("slantMesh")
+            << "Unknown mountainType " << mountainType
+            << exit(FatalError);
+    }
+
+	forAll(newPoints, pointIdx)
+	{
+        scalar x = newPoints[pointIdx].x();
+        scalar z = newPoints[pointIdx].z();
+        scalar h = mountain->heightAt(x);
+        if (z < h)
+        {
+            newPoints[pointIdx].z() = h;
+        }
+    }
+
+    newPoints.write();
+}
