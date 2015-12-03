f47c60830d45dfdbbb27302aa841c4acaacad76b
Hilary Weller <h.weller@reading.ac.uk>
2015-12-03 11:37:01 +0000

    Put all advectionFoam applications into a single directory and added a couple of new ones (Crank-Nicholson and one with a transient deformational velocity field on a cylinder)

---
+++ applications/solvers/advection/scalarDeformationFoam/scalarDeformationFoam.C
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    scalarDeformationFoam
+
+Description
+    Solves a transport equation for a passive scalar using Implicit
+    time-stepping for the evolving velocity field in a cylindrical domain 
+    for deformational flow
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #define dt runTime.deltaT()
+    #include "readDeformationalAdvectionDict.H"
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating scalar transport\n" << endl;
+
+    #include "CourantNo.H"
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << endl;
+
+        #include "calculatePhi.H"
+        #include "CourantNo.H"
+
+        for (int corr = 0; corr < 4; corr++)
+        {
+            solve
+            (
+                fvm::ddt(T)
+              + 0.5*fvm::div(phi, T)
+              + 0.5*divPhiT.oldTime()
+            );
+        }
+        divPhiT = fvc::div(phi, T);
+
+        Info << " T goes from " << min(T.internalField()) << " to "
+             << max(T.internalField()) << nl << endl;
+        runTime.write();
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
