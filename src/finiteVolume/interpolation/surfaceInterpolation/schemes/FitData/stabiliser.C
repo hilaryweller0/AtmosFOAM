aa72cbd27367aaa15f2aa02eec2f872bbe60d0a8
James Shaw <js102@zepler.net>
2016-04-04 09:54:50 +0100

    Add notes for choosing upwind weight when unstable

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/stabiliser.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/stabiliser.C
@@ -1,5 +1,7 @@
 #include "stabiliser.H"
 
+// TODO: maybe change signature to accept the fitWeights and scalarRectangularMatrix
+// then it will be easier to construct weightedMatrices ourselves
 bool Foam::stabiliser::stabilise
 (
     const weightedMatrix& matrix,
@@ -20,6 +22,15 @@ bool Foam::stabiliser::stabilise
         m->populate(c);
 //        Info << "*** coeffs (" << columns << " terms) " << c << endl;
         columns--;
+    }
+
+    if (!c.stable())
+    {
+        // create new weightedMatrix with upwind weight, no downwind weight
+        // eventually, we could try removing the downwind weight gradually (from 5 to 1)
+        // stopping when stability is achieved.  this should give better accuracy
+        // because we've seen in test results that errors are smaller when central weights
+        // are used compared to just upwind weighting.
     }
 
     coefficients.copyFrom(c);
