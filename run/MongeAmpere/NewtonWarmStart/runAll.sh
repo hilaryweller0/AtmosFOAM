78451d0aa7a90dece70e24c68afa634812e5354d
Hilary Weller <h.weller@reading.ac.uk>
2016-04-07 14:41:44 +0100

    Reverting to a previous version of scalarDeformationWithGhosts without a separate solver tolerance for Tfinal and updating run time settings in run/MongeAmpere/NewtonWarmStart

--- run/MongeAmpere/NewtonWarmStart/runAll.sh
+++ run/MongeAmpere/NewtonWarmStart/runAll.sh
@@ -3,9 +3,14 @@
 rm -rf core constant [0-9]*
 blockMesh
 mkdir -p constant/rMesh 0
+ln -s ../../gmtDicts constant/gmtDicts
 cp init0/Phi 0
 cp -r constant/polyMesh constant/rMesh
 NEWTON2D-pab >& log &
 tail -f log
 
 # next edit system/controlDict so that endTime is 100 and it will restart from time step 50 
+
+gmtFoam -latestTime -region rMesh mesh
+gv [1-9]*/mesh.pdf &
+
