63e9bec86a3bf0bffe2ddf58bc2585ae7690d957
Hilary Weller <h.weller@reading.ac.uk>
2016-05-13 08:50:00 +0100

    Two models of the shallow water equations on a beta-plane, one semi-implicit (shallowWaterFoamBeta_C) and one fully explicit (shallowWaterFoamBeta_C_explicit). Test cases can be initialised as a geostrophically balanced jet using setPlaneJet. A sample test case in in run/shallowWater/shallowWaterFoamBeta_C

---
+++ src/shallowWater/BCs/geostrophicZeroFluxFvPatchScalarField.C
@@ -0,0 +1,150 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "geostrophicZeroFluxFvPatchScalarField.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+geostrophicZeroFluxFvPatchScalarField::
+geostrophicZeroFluxFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(p, iF)
+{}
+
+
+geostrophicZeroFluxFvPatchScalarField::
+geostrophicZeroFluxFvPatchScalarField
+(
+    const geostrophicZeroFluxFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    fixedGradientFvPatchScalarField(ptf, p, iF, mapper)
+{}
+
+
+geostrophicZeroFluxFvPatchScalarField::
+geostrophicZeroFluxFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary&
+)
+:
+    fixedGradientFvPatchScalarField(p, iF)
+{
+    fvPatchField<scalar>::operator=(patchInternalField());
+    gradient() = 0.0;
+}
+
+
+geostrophicZeroFluxFvPatchScalarField::
+geostrophicZeroFluxFvPatchScalarField
+(
+    const geostrophicZeroFluxFvPatchScalarField& wbppsf
+)
+:
+    fixedGradientFvPatchScalarField(wbppsf)
+{}
+
+
+geostrophicZeroFluxFvPatchScalarField::
+geostrophicZeroFluxFvPatchScalarField
+(
+    const geostrophicZeroFluxFvPatchScalarField& wbppsf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(wbppsf, iF)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void geostrophicZeroFluxFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const dictionary& environmentalProperties
+        = db().lookupObject<IOdictionary>("environmentalProperties");
+
+    dimensionedScalar g(environmentalProperties.lookup("g"));
+    dimensionedScalar beta(environmentalProperties.lookup("beta"));
+
+    const surfaceVectorField& Uf =
+        db().lookupObject<surfaceVectorField>("Uf");
+    fvsPatchField<vector> Ufp =
+        patch().patchField<surfaceVectorField, vector>(Uf);
+
+    const surfaceVectorField& Cf = Uf.mesh().Cf();
+    fvsPatchField<vector> Cfp =
+        patch().patchField<surfaceVectorField, vector>(Cf);
+
+    gradient() = beta.value()/g.value()*Cfp.component(vector::Y)*
+        ((Ufp ^ vector(0.,0.,1.)) & patch().nf());
+    
+    fixedGradientFvPatchScalarField::updateCoeffs();
+}
+
+
+void geostrophicZeroFluxFvPatchScalarField::write(Ostream& os) const
+{
+    fixedGradientFvPatchScalarField::write(os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    geostrophicZeroFluxFvPatchScalarField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
