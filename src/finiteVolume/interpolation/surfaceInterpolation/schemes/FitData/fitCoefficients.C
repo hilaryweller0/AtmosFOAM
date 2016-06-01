569ee72afa286cd5115fa203f079d4cea0a0bc07
James Shaw <js102@zepler.net>
2016-06-01 16:56:47 +0000

    Revert "Revert "tidy up interfaces for stabiliser""
    This reverts commit a3c7c001452a5134d8c31c3b87af619acac88060.

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/fitCoefficients.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/fitCoefficients.C
@@ -80,15 +80,14 @@ void Foam::fitCoefficients::applyCorrection(const bool goodFit)
     }
 }
 
-bool Foam::fitCoefficients::stable(const localStencil& stencil) const
+bool Foam::fitCoefficients::stable() const
 {
     scalar upwind = coefficients[0];                                            
     scalar downwind = coefficients[1];
-    scalar magSumOfUpwindUpwind = 0.0;
 
     for (int i=2; i < coefficients.size(); i++)
     {
-        if (coefficients[0] < mag(coefficients[i]) + downwind) return false;
+        if (upwind < mag(coefficients[i]) + downwind) return false;
     }
 
     return true;
