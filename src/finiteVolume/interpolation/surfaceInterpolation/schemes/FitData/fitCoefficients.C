22896b1b5fb896edc99bf4c7626b4072d8ce32f4
James Shaw <js102@zepler.net>
2016-07-11 11:30:23 +0100

    try central_are_largest from stability analysis

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/fitCoefficients.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/fitCoefficients.C
@@ -100,10 +100,16 @@ bool Foam::fitCoefficients::stable() const
 
 bool Foam::fitCoefficients::central_are_largest() const
 {
-    scalar smallest_central_coefficient = min(coefficients[0], coefficients[1]);
+/*    scalar smallest_central_coefficient = min(coefficients[0], coefficients[1]);
     for (int i=2; i < coefficients.size(); i++)
     {
         if (-coefficients[i]+0.01 > smallest_central_coefficient) return false;
+    }
+    return true;
+    */
+    for (int i=2; i < coefficients.size(); i++)
+    {
+        if (coefficients[i] >= coefficients[0] - coefficients[1]) return false;
     }
     return true;
 }
