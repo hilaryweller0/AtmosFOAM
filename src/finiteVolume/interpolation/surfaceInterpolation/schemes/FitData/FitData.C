3456e5462974dbefd16232899338f2052728fe30
James Shaw <js102@zepler.net>
2016-07-25 18:04:12 +0000

    use minSingularValueThreshold=0.1 for better accuracy

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/FitData.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/FitData/FitData.C
@@ -131,8 +131,7 @@ autoPtr<fitResult> Foam::FitData<FitDataType, ExtendedStencil, Polynomial>::calc
     fitWeights weights(C.size());
     weights.setCentralWeight(centralWeight_, pureUpwind);
 
-    PolynomialFit2<Polynomial> polynomialFit(dim_, 0.1);
-    PolynomialFit2<Polynomial> polynomialFitUnstable(dim_, 1e-9);
+    PolynomialFit2<Polynomial> polynomialFitUnstable(dim_, 0.1);
 
     const Basis basis(idir, jdir, kdir);
     const localStencil stencil(C, p0, basis);
