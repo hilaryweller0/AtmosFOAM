dcba61f714f1f44dbaf9f1ebb81be0bc634e9a49
James Shaw <js102@zepler.net>
2016-08-15 16:35:24 +0100

    add libprescribedFields

---
+++ src/prescribedFields/deformationalNonDivergentGeodesic.C
@@ -0,0 +1,32 @@
+#include "deformationalNonDivergentGeodesic.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "polarPoint.H"
+
+defineTypeNameAndDebug(deformationalNonDivergentGeodesicVelocityField, 0);
+addToRunTimeSelectionTable(velocityField, deformationalNonDivergentGeodesicVelocityField, dict);
+
+deformationalNonDivergentGeodesicVelocityField::deformationalNonDivergentGeodesicVelocityField(const dictionary& dict)
+:
+radius("radius", dimLength, dict.lookupOrDefault<scalar>("radius", scalar(6.3712e6)))
+{};
+
+vector deformationalNonDivergentGeodesicVelocityField::streamfunctionAt
+(
+        const point& p,
+        const Time& t
+) const
+{
+    const dimensionedScalar T = t.endTime();
+
+    const polarPoint& polarp = convertToPolar(p);
+    const scalar lat = polarp.lat();
+    const scalar lon = polarp.lon();
+
+    const dimensionedScalar lonPrime = lon - 2 * M_PI * t / T;
+
+    const dimensionedScalar psi = 10 * radius / T * 
+            sqr(Foam::sin(2*lonPrime)) * sqr(Foam::cos(lat)) * Foam::cos(M_PI*t/T) - 
+            2*M_PI*radius/T * Foam::sin(lat);
+    return p/mag(p) * psi.value();
+}
