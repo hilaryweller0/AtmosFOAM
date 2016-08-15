9a213dd1603aaa836e960cd90a30d36d09647a1d
James Shaw <js102@zepler.net>
2016-08-15 10:27:23 +0100

    read from velocityFieldDict, debug divu and w

--- applications/utilities/preProcessing/setGeodesicSolidBodyVelocityField/setGeodesicSolidBodyVelocityField.C
+++ applications/utilities/preProcessing/setGeodesicSolidBodyVelocityField/setGeodesicSolidBodyVelocityField.C
@@ -9,8 +9,21 @@ int main(int argc, char *argv[])
     #include "createMesh.H"
     #include "createFields.H"
 
-    dimensionedScalar radius("radius", dimLength, 6.3712e6);
-    dimensionedScalar timeScale = runTime.endTime();
+    IOdictionary velocityFieldDict
+    (
+        IOobject
+        (
+            "velocityFieldDict",
+            mesh.time().constant(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::NO_WRITE
+        )
+    );
+
+    const dimensionedScalar radius("radius", dimLength, velocityFieldDict.lookupOrDefault<scalar>("radius", scalar(6.3712e6)));
+    const scalar alpha0(velocityFieldDict.lookupOrDefault<scalar>("tilt", scalar(0)));
+    const dimensionedScalar timeScale = runTime.endTime();
 
     forAll(Uf, faceI)
     {
@@ -18,7 +31,6 @@ int main(int argc, char *argv[])
         const scalar lat = polarp.lat();
         const scalar lon = polarp.lon();
 
-        scalar alpha0 = 0;
         dimensionedScalar u0 = 2 * M_PI * radius / timeScale;
 
         dimensionedScalar u = u0 * (Foam::cos(alpha0) * Foam::cos(lat) + Foam::sin(alpha0) * Foam::cos(lon) * Foam::sin(lat));
@@ -30,6 +42,14 @@ int main(int argc, char *argv[])
         Uf[faceI] = localWind.toCartesian(p);
     }
     Uf.write();
+
+    Info << "Calculating the divergence field to check that it is zero" << endl;
+    volScalarField divu("divu", fvc::div(Uf & mesh.Sf()));
+    divu.write();
+
+    Info << "Calculating the radial wind component w to check that it is zero" << endl;
+    surfaceScalarField w("w", Uf & (mesh.Cf()/mag(mesh.Cf())));
+    w.write();
 
     return EXIT_SUCCESS;
 }
