47f2f179c88af787e6ad7f6b24a3b8e057132167
James Shaw <js102@zepler.net>
2016-08-24 12:00:30 +0100

    define schaerRadial at initial position

--- src/prescribedFields/schaerRadialTracerField.C
+++ src/prescribedFields/schaerRadialTracerField.C
@@ -8,7 +8,7 @@ schaerRadialTracerField::schaerRadialTracerField(const dictionary& dict)
 :
 rho0(dict.lookupOrDefault<scalar>("maxMagnitude", scalar(1))),
 p0(dict.lookupOrDefault<point>("centre", point(-50e3, 0, 9e3))),
-A(dict.lookupOrDefault<vector>("halfWidth", vector(25e3, 0, 3e3)))
+A(dict.lookupOrDefault<vector>("halfWidth", vector(25e3, 1, 3e3)))
 {};
 
 scalar schaerRadialTracerField::tracerAt
@@ -17,5 +17,15 @@ scalar schaerRadialTracerField::tracerAt
         const Time& t
 ) const
 {
-    return 0;
+    vector d = p - p0;
+    scalar r = mag(vector(d.x()/A.x(), 0, d.z()/A.z()));
+
+    if (r <= 1)
+    {
+        return rho0*sqr(Foam::cos(M_PI*r/2));
+    }
+    else
+    {
+        return 0;
+    }
 }
