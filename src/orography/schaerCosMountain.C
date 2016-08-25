8a0197aa833d17f1270a846e356681cc0a964617
James Shaw <js102@zepler.net>
2016-08-25 13:37:18 +0100

    implement schaerCosMountain, fix up slantMesh

--- src/orography/schaerCosMountain.C
+++ src/orography/schaerCosMountain.C
@@ -6,12 +6,17 @@ addToRunTimeSelectionTable(mountain, schaerCosMountain, dict);
 
 schaerCosMountain::schaerCosMountain(const dictionary& dict)
 :
+h0("peakHeight", dimLength, readScalar(dict.lookup("peakHeight"))),
 a(readScalar(dict.lookup("halfWidth"))),
-h0(readScalar(dict.lookup("peakHeight"))),
 lambda(readScalar(dict.lookup("wavelength")))
 {};
 
 dimensionedScalar schaerCosMountain::heightAt(const point& p) const
 {
-    return dimensionedScalar("height", dimLength, scalar(0));
+    dimensionedScalar h("height", dimLength, scalar(0));
+    if (mag(p.x()) <= a)
+    {
+        h = h0 * sqr(Foam::cos(M_PI * p.x() / lambda)) * sqr(Foam::cos(0.5*M_PI*p.x()/a));
+    }
+    return h;
 }
