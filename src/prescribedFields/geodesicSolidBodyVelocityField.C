ed8bfbf6e4e02fc76ded2c90c370dcf15ee1fef7
James Shaw <js102@zepler.net>
2016-09-01 16:48:03 +0100

    add geodesicSolidBodyVelocityField

---
+++ src/prescribedFields/geodesicSolidBodyVelocityField.C
@@ -0,0 +1,31 @@
+#include "geodesicSolidBodyVelocityField.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "polarPoint.H"
+
+defineTypeNameAndDebug(geodesicSolidBodyVelocityField, 0);
+addToRunTimeSelectionTable(velocityField, geodesicSolidBodyVelocityField, dict);
+
+geodesicSolidBodyVelocityField::geodesicSolidBodyVelocityField(const dictionary& dict)
+:
+radius("radius", dimLength, dict.lookupOrDefault<scalar>("radius", scalar(6.3712e6))),
+alpha(dict.lookupOrDefault<scalar>("tilt", scalar(0)))
+{};
+
+vector geodesicSolidBodyVelocityField::streamfunctionAt
+(
+        const point& p,
+        const Time& t
+) const
+{
+    const scalar alpha = 0;
+    const scalar u0 = 2 * M_PI * radius.value() / t.endTime().value();
+    const polarPoint& polarp = convertToPolar(p);
+    const scalar lat = polarp.lat();
+    const scalar lon = polarp.lon();
+
+    const scalar psi = - u0 * Foam::sin(lat) * Foam::cos(alpha) - Foam::cos(lon) * Foam::cos(lat) * Foam::sin(alpha);
+
+    return p/mag(p) * radius.value() * psi;
+}
+
