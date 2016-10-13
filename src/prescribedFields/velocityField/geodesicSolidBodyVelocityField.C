116cd49536b6d821eb0c9f6da7fed6e03f586669
James Shaw <js102@zepler.net>
2016-10-13 16:20:12 +0100

    don't use annular sector approximation, use same approximation as for planar

--- src/prescribedFields/velocityField/geodesicSolidBodyVelocityField.C
+++ src/prescribedFields/velocityField/geodesicSolidBodyVelocityField.C
@@ -27,7 +27,7 @@ vector geodesicSolidBodyVelocityField::streamfunctionAt
 
     const scalar psi = - u0 * (Foam::sin(lat) * Foam::cos(alpha) - Foam::cos(lon) * Foam::cos(lat) * Foam::sin(alpha));
 
-    return p/mag(p) * psi;
+    return p/mag(p) * psi * radius.value();
 }
 
 point geodesicSolidBodyVelocityField::initialPositionOf
