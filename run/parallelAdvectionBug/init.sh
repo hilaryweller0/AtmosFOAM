82977e63a94b4f87c2aec31a0668f839c64b9741
James Shaw <js102@zepler.net>
2016-12-21 10:29:21 +0000

    further simplify parallelAdvectionBug test case

--- run/parallelAdvectionBug/init.sh
+++ run/parallelAdvectionBug/init.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
-rm -rf [0-9]* constant/polyMesh processor* *.dat *.cpt constant/*Terms
+rm -rf 1 constant/polyMesh processor* *.dat *.cpt constant/*Terms
 blockMesh
-setInitialTracerField
+#setInitialTracerField
 setVelocityField
 decomposePar
-mpirun --hostfile machines -np 2 advectionFoam -parallel
+mpirun --hostfile machines -np 2 advectionFoam -parallel -forwardEuler
 reconstructPar
 cp 0/T 0/T.par
 mv 1/T 1/T.par
-mv 2/T 2/T.par
-advectionFoam
+advectionFoam -forwardEuler
 sumFields -scale0 1 -scale1 -1 1 T_diff 1 T 1 T.par
-sumFields -scale0 1 -scale1 -1 2 T_diff 2 T 2 T.par
 gmtFoam -time 1 T_diff
 gv 1/T_diff.pdf &
 
