f617d8bf8f9931085a00af43e89631d7c6ea6fac
James Shaw <js102@zepler.net>
2017-02-03 17:35:06 +0000

    linearUpwind is slightly better for advectiveFoamF test

--- run/advectionCP/run.cp.sh
+++ run/advectionCP/run.cp.sh
@@ -12,10 +12,10 @@ sumFields -scale0 1 -scale1 -1 10000 Tf_diff 10000 Tf 10000 Tf_analytic
 
 globalSum -time 10000 Tf_diff
 mv globalSumTf_diff.dat 10000
-tail -n1 10000/globalSumTf_diff.dat |  cut -d' ' -f3 > 10000/l2errorTf_diff.txt
+tail -n1 10000/globalSumTf_diff.dat | cut -d' ' -f3 > 10000/l2errorTf_diff.txt
 
 globalSum -time 10000 Tf_analytic
 mv globalSumTf_analytic.dat 10000
-tail -n1 10000/globalSumTf_analytic.dat |  cut -d' ' -f3 > 10000/l2errorTf_analytic.txt
+tail -n1 10000/globalSumTf_analytic.dat | cut -d' ' -f3 > 10000/l2errorTf_analytic.txt
 
-python3 -c "print(`paste -d'/' 10000/l2errorTf_diff.txt 10000/l2errorTf_analytic.txt`)" > 10000/l2errorT.txt
+python3 -c "print(`paste -d'/' 10000/l2errorTf_diff.txt 10000/l2errorTf_analytic.txt`)" > 10000/l2errorTf.txt
