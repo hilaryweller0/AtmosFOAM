c543a8594512139c5b6dec22c9f614f21c5f2e3f
James Shaw <js102@zepler.net>
2017-03-13 11:36:21 +0000

    add other standing waves test case from Arakawa&Konor1996

---
+++ run/arakawaKonorStripes/run.cp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+rm -rf [0-9]*
+blockMesh
+
+setInitialTracerField
+mv 0/T 0/theta.anom
+mv 0/Tf 0/thetaf.anom
+
+setTheta
+setTheta -CP
+
+setExnerBalancedH -noInterpolate
+
+mv 0/theta 0/theta.bg
+mv 0/thetaf 0/thetaf.bg
+
+sumFields -scale0 1 -scale1 1 0 theta 0 theta.bg 0 theta.anom
+sumFields -scale0 1 -scale1 1 0 thetaf 0 thetaf.bg 0 thetaf.anom
+
+cp init_0/Uf 0/Uf
+set +e
+exnerFoamH
+#exnerFoamCPinterpGrad
+
+for t in [0-9]*
+do
+	sumFields -scale0 1 -scale1 -1 $t theta_diff $t theta 0 theta.bg
+#	sumFields -scale0 1 -scale1 -1 $t thetaf_diff $t thetaf 0 thetaf.bg
+done
