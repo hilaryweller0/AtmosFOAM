cafdb0fddc11172ce0f67ac3a9fbd1fe5ffcd251
James Shaw <js102@zepler.net>
2017-03-21 11:55:30 +0000

    candidate polynomial generator now works in 2D/3D

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/cubicUpwindCPCFit/genPolynomialCandidates.py
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/cubicUpwindCPCFit/genPolynomialCandidates.py
@@ -8,26 +8,18 @@ def each_degree_le_max(e):
 
     return True
 
-def format(term):
-    s = ""
-    algebra = ["x", "y", "z"]
-    for i, t in enumerate(term):
-        if t == 1:
-            s += algebra[i]
-        elif t > 1:
-            s += algebra[i] + "^" + str(t)
-    return s
+def dense(s):
+    s = set(s)
 
-def ordered(pair):
-    pair = tuple(pair)
-    a = pair[0]
-    b = pair[1]
-
-    for i, x in enumerate(a):
-        if x > b[i]:
+    for upper in s:
+        lower_order_terms = set(itertools.product(*ranges(upper)))
+        if not lower_order_terms.issubset(s):
             return False
 
     return True
+
+def ranges(upper):
+    return [tuple(range(x+1)) for x in upper]
 
 def between_bounds(e, a, b):
     for i, x in enumerate(e):
@@ -36,28 +28,34 @@ def between_bounds(e, a, b):
 
     return True
 
-max_degrees = (3, 2) # x^3, y^2
+def format_polynomial(polynomial):
+    return " + ".join([format_term(t) for t in sorted(polynomial, key=lambda p: sum(p))])
+
+def format_term(term):
+    s = ""
+    algebra = ["x", "y", "z"]
+    for i, t in enumerate(term):
+        if t == 1:
+            s += algebra[i]
+        elif t > 1:
+            s += algebra[i] + "^" + str(t)
+
+    if s == "":
+        s = "constant"
+
+    return s
+
+max_degrees = (3, 2) # x^3, y^2, z^2
 
 exponents = itertools.product(range(max(max_degrees)+1), repeat=len(max_degrees))
 exponents = [e for e in exponents if sum(e) <= max(max_degrees) and each_degree_le_max(e)]
 
 M = set(exponents)
 
-pairs = itertools.combinations(M, 2)
-dense_subsets = []
+for length in range(2,len(M)+1):
+    subsets = itertools.combinations(M, length)
+    dense_subsets = [s for s in subsets if dense(s)]
+    print("\n".join([format_polynomial(s) for s in dense_subsets]))
 
-for pair in pairs:
-    if ordered(pair):
-        a, b = pair
-    elif ordered(reversed(pair)):
-        b, a = pair
-    else:
-        continue
-
-    print("bounds", a, b)
-    S = {e for e in M if (e in pair) or between_bounds(e, a, b)}
-    print(S)
-    print()
-    # now, filter M using lower bound 'a' and upper bound 'b'
-
-# TODO: remove yz^2 and y^2z ?
+# TODO: stricter constraints for including xy/xz/yz?
+# TODO: remove yz^2 and y^2z?
