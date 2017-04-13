3836057af6a40dc790a95b950eda0041ca2967e0
Hilary Weller <h.weller@reading.ac.uk>
2017-04-13 19:18:55 +0100

    Changing setBalancedHeight for the shallow water equations for consistency with new moving mesh shallow water models in AMMM

---
+++ applications/utilities/preProcessing/setBalancedHeightRC/setBalancedHeight.C
@@ -0,0 +1,108 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    setBalancedHeightRC
+
+Description
+    Find height field for the shallow water equations in balance with a
+    given velocity field with velocity, U, defined at cell centres
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    Foam::argList::addOption
+    (
+        "region",
+        "meshRegion",
+        "specify a non-default region to plot"
+    );
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #define dt runTime.deltaT()
+    #define rMesh mesh
+    // Check for plotting non-default region
+    const string meshRegion = args.optionFound("region") ?
+                              args.optionRead<string>("region") :
+                              fvMesh::defaultRegion;
+
+    Info << "Create mesh for time = " << runTime.timeName() <<  " region "
+         << meshRegion << endl;
+
+    fvMesh mesh
+    (
+        Foam::IOobject
+        (
+            meshRegion, runTime.timeName(), runTime, IOobject::MUST_READ
+        )
+    );
+
+    // Read in and create variables and fields
+    #include "readEnvironmentalProperties.H"
+    #include "createFields.H"
+
+    const dictionary& itsDict = mesh.solutionDict().subDict("initialisation");
+    const int maxIters = itsDict.lookupOrDefault<int>("maxIters", 100);
+   
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    bool converged = false;
+    for(label iter = 0; iter < maxIters && !converged; iter++)
+    {
+        Info << "Iteration " << iter << endl;
+        hf = fvc::interpolate(h);
+        phi = hf*(Uf & mesh.Sf());
+        
+        surfaceScalarField phiTmp = dt*fvc::flux
+        (
+          - fvc::div(phi,U)/h
+          - (twoOmega ^ U)
+        );
+        
+        fvScalarMatrix hEqn
+        (
+            fvc::div(phiTmp) - fvm::laplacian(dt*g, h, "laplacian(h)")
+        );
+        hEqn.setReference(0, h[0]);
+        converged = hEqn.solve(mesh.solver(h.name())).nIterations() == 0;
+    }
+
+    h.write();
+    
+    volVectorField hU("hU", h*U);
+    hU.write();
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
+
