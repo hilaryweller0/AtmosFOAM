efeccf97c79d02a383a13547889224d81bc67d16
James Shaw <js102@zepler.net>
2017-04-24 09:34:02 +0100

    set up A&K radiation initial conditions

---
+++ src/prescribedFields/tracerField/arakawaKonorRadiation.C
@@ -0,0 +1,30 @@
+#include "arakawaKonorRadiation.H"
+#include "addToRunTimeSelectionTable.H"
+
+defineTypeNameAndDebug(arakawaKonorRadiationTracerField, 0);
+addToRunTimeSelectionTable(tracerField, arakawaKonorRadiationTracerField, dict);
+
+arakawaKonorRadiationTracerField::arakawaKonorRadiationTracerField
+(
+    const dictionary& dict,
+    const advectable& velocityField
+)
+:
+tracerField(velocityField)
+{};
+
+scalar arakawaKonorRadiationTracerField::tracerAt
+(
+        const point& p,
+        const Time& t
+) const
+{
+    if (p.z() >= 3900 - SMALL && p.z() <= 4550 + SMALL)
+    {
+        return 10;
+    }
+    else
+    {
+        return 0;
+    }
+}
