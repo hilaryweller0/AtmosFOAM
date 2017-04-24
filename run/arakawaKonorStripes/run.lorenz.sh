701f04e5e7c1e6063cdb304c2cabc81ccb48372d
James Shaw <js102@zepler.net>
2017-04-24 11:44:05 +0100

    split lorenz and cp runs for stripes test

---
+++ run/arakawaKonorStripes/run.lorenz.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+rm -rf [0-9]*
+blockMesh
+
+setInitialTracerField
+mv 0/T 0/theta.anom
+
+setTheta
+
+setExnerBalancedH
+
+mv 0/theta 0/theta.bg
+
+sumFields -scale0 1 -scale1 1 0 theta 0 theta.bg 0 theta.anom
+
+cp init_0/Uf 0/Uf
+set +e
+exnerFoamH
+
+for t in [0-9]*
+do
+	sumFields -scale0 1 -scale1 -1 $t theta_diff $t theta 0 theta.bg
+done
