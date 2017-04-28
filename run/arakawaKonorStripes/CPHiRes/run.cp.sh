b73597eae17cadf666e1b736c320c6b0f8866ae7
James Shaw <js102@zepler.net>
2017-04-28 13:59:28 +0100

    add hi/lo res variants of arakawaKonorStripes test

--- run/arakawaKonorStripes/CPHiRes/run.cp.sh
+++ run/arakawaKonorStripes/CPHiRes/run.cp.sh
@@ -5,19 +5,22 @@ blockMesh
 
 setInitialTracerField
 mv 0/T 0/theta.anom
+mv 0/Tf 0/thetaf.anom
 
-setTheta
+setTheta -CP
 
-setExnerBalancedH
+setExnerBalancedH -noInterpolate
 
 mv 0/theta 0/theta.bg
+mv 0/thetaf 0/thetaf.bg
 
 sumFields -scale0 1 -scale1 1 0 theta 0 theta.bg 0 theta.anom
+sumFields -scale0 1 -scale1 1 0 thetaf 0 thetaf.bg 0 thetaf.anom
 
 cp init_0/Uf 0/Uf
 createSpongeLayer
 set +e
 
 decomposePar -force -constant
-mpirun --hostfile machines -np 2 exnerFoamH -parallel
+mpirun --hostfile machines -np 2 exnerFoamCPinterpGrad -parallel
 ./diff.sh
