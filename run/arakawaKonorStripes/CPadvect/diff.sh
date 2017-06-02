0d9af15f5544691ada886557ae4efef2fcc0b2c7
James Shaw <js102@zepler.net>
2017-06-02 17:30:16 +0100

    remove edgeGrading, mpirun, CPadvection still going wrong

--- run/arakawaKonorStripes/CPadvect/diff.sh
+++ run/arakawaKonorStripes/CPadvect/diff.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 reconstructPar
 
+setAnalyticTracerField
+
 for t in [0-9]*
 do
-	sumFields -scale0 1 -scale1 -1 $t theta_diff $t theta 0 theta.bg
-	sumFields -scale0 1 -scale1 -1 $t thetaf_diff $t thetaf 0 thetaf.bg
+	sumFields -scale0 1 -scale1 -1 $t Tf_diff $t Tf $t Tf_analytic
+	sumFields -scale0 1 -scale1 -1 $t T_diff $t T $t T_analytic
 done
 
