b4efd95f25e7da1f53e1b06fde457270000cbe07
Will <will.a.m@hotmail.co.uk>
2017-06-13 13:24:10 +0100

    More minor modifications to moisture advection test cases.

--- src/prescribedFields/tracerField/schaerRadialDampedField.C
+++ src/prescribedFields/tracerField/schaerRadialDampedField.C
@@ -15,6 +15,7 @@ profileType(dict.lookupOrDefault<string>("profileType", string("default"))),
 rho0(dict.lookupOrDefault<scalar>("maxMagnitude", scalar(0.001))),
 rhoAir(dict.lookupOrDefault<scalar>("rhoAir", scalar(1))),
 p0(dict.lookupOrDefault<point>("centre", point(-50e3, 0, 9e3))),
+p1(dict.lookupOrDefault<point>("ground", point(0, 0, -1000))),
 A(dict.lookupOrDefault<vector>("halfWidth", vector(25e3, 1, 3e3)))
 {};
 
@@ -25,6 +26,7 @@ scalar schaerRadialDampedField::tracerAt
 ) const
 {
     vector d = p - p0;
+    vector e = p - p1;
     scalar r = mag(vector(d.x()/A.x(), 0, d.z()/A.z()));
 
     if (r <= 1)
@@ -34,22 +36,26 @@ scalar schaerRadialDampedField::tracerAt
         {
             return rho0*sqr(Foam::cos(M_PI*r/2)) + rhoAir;
         }
-        else if (profileType == "r")
+        else if (profileType == "rvs")
         {
-            return rho0/rhoAir*sqr(Foam::cos(M_PI*r/2));
+            scalar theta0 = 300;
+            scalar g = 9.81;
+            scalar c_p = 1004;
+            scalar muv = 0.018;
+            scalar mud = 0.029;
+    
+            scalar T = 300*Foam::exp(-g*e.z()/(c_p*theta0));
+            scalar P = 100000*Foam::exp(-g*e.z()/(c_p*theta0));
+            scalar es = 611.2*Foam::exp( 17.67*(T-273.15)/(T-29.65) );
+            
+            return rho0 * muv/mud * es/(P-es) + rhoAir;
+            //return rho0/rhoAir*sqr(Foam::cos(M_PI*r/2));
         }
         else if (profileType == "q")
         {
             return rho0*sqr(Foam::cos(M_PI*r/2))/(rhoAir + rho0*sqr(Foam::cos(M_PI*r/2)));
         }
-        else
-        {
-            return rhoAir;
-        }
-    }
-    else
-    {
-        if (profileType == "default")
+        else if (profileType == "block")
         {
             return rhoAir;
         }
@@ -57,5 +63,9 @@ scalar schaerRadialDampedField::tracerAt
         {
             return 0;
         }
+    }
+    else
+    {
+        return rhoAir;
     }
 }
