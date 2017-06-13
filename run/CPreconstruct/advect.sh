19f63c632652c95dfa6b7f18e09809b4dccafe38
James Shaw <js102@zepler.net>
2017-06-13 16:25:32 +0100

    drive out non-conservation bug in advectionConservativeF

--- run/CPreconstruct/advect.sh
+++ run/CPreconstruct/advect.sh
@@ -3,9 +3,12 @@ set -e
 rm -rf [0-9]*
 blockMesh
 mkdir 0
-cp init_0/Tf 0/Tf
+#cp init_0/Tf 0/Tf
+setInitialTracerField
 cp init_0/Uf 0/Uf
 advectionConservativeF
+gmtFoam -time 0,1 Tf
+gmtFoam -time 1 TfRaw
 zeroVerticalFaces Tf
+cp 0/Tf 0/TfRaw
 globalSum Tf
-gmtFoam -time 0,1 mesh
--- run/CPreconstruct/reconstruct.sh
+++ run/CPreconstruct/reconstruct.sh
@@ -3,6 +3,6 @@ set -e
 rm -rf [0-9]*
 blockMesh
 mkdir 0
-cp init_0/Tf 0/Tf
+setInitialTracerField
 testCPreconstruction
 gmtFoam -time 0,1 mesh
