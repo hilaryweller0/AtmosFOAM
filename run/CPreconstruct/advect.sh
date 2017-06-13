dc6d74717575ce7e7adcd72d3f587831396315d4
James Shaw <js102@zepler.net>
2017-06-13 11:31:38 +0100

    make test 3x2 in size

--- run/CPreconstruct/advect.sh
+++ run/CPreconstruct/advect.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 rm -rf [0-9]*
+blockMesh
 mkdir 0
 cp init_0/Tf 0/Tf
 cp init_0/Uf 0/Uf
 advectionConservativeF
+zeroVerticalFaces Tf
+globalSum Tf
+gmtFoam -time 0,1 mesh
--- run/CPreconstruct/reconstruct.sh
+++ run/CPreconstruct/reconstruct.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 rm -rf [0-9]*
+blockMesh
 mkdir 0
 cp init_0/Tf 0/Tf
 testCPreconstruction
