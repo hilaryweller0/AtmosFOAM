9208264c45bb64f1a5aa1fc475adf4dd5e36c567
James Shaw <js102@zepler.net>
2017-06-13 10:14:11 +0100

    add advection

--- run/CPreconstruct/advect.sh
+++ run/CPreconstruct/advect.sh
@@ -3,4 +3,5 @@ set -e
 rm -rf [0-9]*
 mkdir 0
 cp init_0/Tf 0/Tf
-testCPreconstruction
+cp init_0/Uf 0/Uf
+advectionConservativeF
---
+++ run/CPreconstruct/reconstruct.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+rm -rf [0-9]*
+mkdir 0
+cp init_0/Tf 0/Tf
+testCPreconstruction
+gmtFoam -time 0,1 mesh
