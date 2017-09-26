87f10ba7786a51d7103f38ca3190f19256fa8c9b
James Shaw <js102@zepler.net>
2017-09-26 14:48:29 +0100

    don't store autoPtr<crossableMountain>
    Because we're going to provide two types of analytic solution,
    one that provides only the horizontal offset (the current implementation),
    and another that provides only the vertical offset (needed for
    thermalAdvect steady-state solution).

--- src/orography/btfVelocityField.C
+++ src/orography/btfVelocityField.C
@@ -8,7 +8,7 @@ btfVelocityField::btfVelocityField(const dictionary& dict)
 :
 u0("speed", dimVelocity, dict.lookupOrDefault<scalar>("speed", scalar(10))),
 H("domainHeight", dimLength, readScalar(dict.lookup("domainHeight"))),
-m(autoPtr<crossableMountain>(dynamic_cast<crossableMountain*>(mountain::New(dict.subDict("mountain")).ptr())))
+m(mountain::New(dict.subDict("mountain")))
 {};
 
 vector btfVelocityField::streamfunctionAt
@@ -35,9 +35,11 @@ point btfVelocityField::initialPositionOf
     const Time& t
 ) const
 {
+    const crossableMountain& mountain = dynamic_cast<const crossableMountain&>(m());
+
     dimensionedScalar x("x", dimLength, p.x());
-    dimensionedScalar t1 = (m->start() - x) / u0;
-    dimensionedScalar t2 = m->timeToCross(u0, H);
+    dimensionedScalar t1 = (mountain.start() - x) / u0;
+    dimensionedScalar t2 = mountain.timeToCross(u0, H);
 
     if (t.value() <= t1.value())
     {
@@ -45,7 +47,7 @@ point btfVelocityField::initialPositionOf
     }
     else if (t.value() >= (t1 + t2).value())
     {
-        return point((x - (m->end() - m->start()) - u0 * (t - t2)).value(), p.y(), p.z());
+        return point((x - (mountain.end() - mountain.start()) - u0 * (t - t2)).value(), p.y(), p.z());
     }
     else
     {
