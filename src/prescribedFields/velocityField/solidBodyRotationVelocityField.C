fcaefb0152a668df1c62fe7bd902f28c31748f12
Hilary Weller <h.weller@reading.ac.uk>
2017-10-21 14:50:41 +0100

    Added inner and outer radii to prescribedFields/velocityField/solidBodyRotationVelocityField

--- src/prescribedFields/velocityField/solidBodyRotationVelocityField.C
+++ src/prescribedFields/velocityField/solidBodyRotationVelocityField.C
@@ -8,7 +8,9 @@ addToRunTimeSelectionTable(velocityField, solidBodyRotationVelocityField, dict);
 solidBodyRotationVelocityField::solidBodyRotationVelocityField(const dictionary& dict)
 :
     rotation_(dict.lookup("solidBodyRotation")),
-    centre_(dict.lookup("centreOfRotation"))
+    centre_(dict.lookup("centreOfRotation")),
+    innerRadius_(dict.lookupOrDefault<scalar>("innerRadius", scalar(-1))),
+    outerRadius_(dict.lookupOrDefault<scalar>("outerRadius", scalar(-1)))
 {};
 
 vector solidBodyRotationVelocityField::streamfunctionAt
@@ -20,8 +22,39 @@ vector solidBodyRotationVelocityField::streamfunctionAt
     // Vector from point p to axis of rotation
     vector d = p - centre_;
     d = d - (d & rotation_)*rotation_/magSqr(rotation_);
+    scalar radius = mag(d);
+    vector streamFunc = vector::zero;
+    
+    if (innerRadius_ <=0 || outerRadius_ <= 0 || radius < innerRadius_)
+    {
+        streamFunc = magSqr(d)*rotation_;
+    }
+    else if (radius < outerRadius_)
+    {
+        streamFunc = rotation_*
+        (
+            sqr(innerRadius_)
+          + 2*innerRadius_/(outerRadius_-innerRadius_)*
+            (
+                outerRadius_*(radius - innerRadius_)
+              - 0.5*(sqr(radius) - sqr(innerRadius_))
+            )
+        );
+    }
+    else
+    {
+        streamFunc = rotation_*
+        (
+            sqr(innerRadius_)
+          + 2*innerRadius_/(outerRadius_-innerRadius_)*
+            (
+                outerRadius_*(outerRadius_-innerRadius_)
+              - 0.5*(sqr(outerRadius_) - sqr(innerRadius_))
+            )
+        );
+    }
 
-    return magSqr(d)*rotation_;
+    return streamFunc;
 }
 
 point solidBodyRotationVelocityField::initialPositionOf
