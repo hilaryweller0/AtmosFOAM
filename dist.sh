aa3b15689038e669862a869a219fffbfa684d6b8
James Shaw <js102@zepler.net>
2018-01-29 15:24:38 +0000

    more allow-insecure-repositories fixes

--- dist.sh
+++ dist.sh
@@ -34,8 +34,7 @@ singularity exec -e $CODENAME.img debuild -i -us -uc -b
 deb-s3 upload --bucket atmosfoam-apt --codename=$CODENAME --component=dev ../atmosfoam_${VERSION}_amd64.deb
 
 # ensure that the new .deb can be installed
-sudo singularity exec -e -w $CODENAME.img apt-get update -qq
-sudo singularity exec -e -w $CODENAME.img apt-get remove highorderfit -y
+sudo singularity exec -e -w $CODENAME.img apt-get update -qq --allow-insecure-repositories
 sudo singularity exec -e -w $CODENAME.img apt-get install atmosfoam=$VERSION -y --allow-unauthenticated --no-install-recommends
 
 git clean -xfd .
