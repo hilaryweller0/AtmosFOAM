dbd39febf05e67c6b64f46c17cd09b32761e55d6
Hiroe Yamazaki <ax915962@swlx9.rdg.ac.uk>
2018-08-19 15:02:05 +0100

    add doubleCylinderMountain

---
+++ src/orography/doubleCylinderMountain.C
@@ -0,0 +1,32 @@
+#include "doubleCylinderMountain.H"
+#include "addToRunTimeSelectionTable.H"
+
+defineTypeNameAndDebug(doubleCylinderMountain, 0);
+addToRunTimeSelectionTable(mountain, doubleCylinderMountain, dict);
+
+doubleCylinderMountain::doubleCylinderMountain(const dictionary& dict)
+:
+    h0_(dict.lookup("peakHeight")),
+    centre_(dict.lookup("mountainCentre")),
+    radius_(dict.lookup("mountainRadius")),
+    h0m_(dict.lookup("peakHeightM")),
+    centrem_(dict.lookup("mountainCentreM")),
+    radiusm_(dict.lookup("mountainRadiusM"))
+{};
+
+dimensionedScalar doubleCylinderMountain::heightAt(const point& p) const
+{
+    dimensionedScalar h("height", dimLength, scalar(0));
+
+    scalar r = horizontalDist(p, centre_);
+    scalar rm = horizontalDist(p, centrem_);
+    if (r < radius_.value())
+    {
+        h = h0_;
+    }
+    if (rm < radius_.value())
+    {
+        h = h0m_;
+    }
+    return h;
+}
