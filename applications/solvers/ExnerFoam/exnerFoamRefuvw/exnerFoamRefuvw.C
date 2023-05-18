8f73d0f044e7aad45cd1b6651bab10212834da6c
Hilary Weller <h.weller@reading.ac.uk>
2023-05-18 10:48:15 +0100

    exnerFoamRefuvw solves perturbation forms of the equations and separate solutions for u, v and w. Convergence is slow for long time steps in test case vSlice/hillWaves/hydro/coarseRef

---
+++ applications/solvers/ExnerFoam/exnerFoamRefuvw/exnerFoamRefuvw.C
@@ -0,0 +1,127 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    exnerFoamRefuvw
+
+Description
+    Transient solver for buoyant, viscous, compressible, non-hydrostatic flow
+    using a simultaneous solution of Exner, theta and phi. 
+    Separate solutions for components of the velocity.
+    Optional turbulence modelling.
+    Optional implicit gravity waves and implicit advection.
+    Removes reference profile.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fluidThermo.H"
+#include "compressibleMomentumTransportModels.H"
+#include "fluidThermophysicalTransportModel.H"
+#include "physicalProperties.H"
+#include "fundamentalConstants.H"
+#include "specie.H"
+#include "perfectGas.H"
+#include "hConstThermo.H"
+#include "constTransport.H"
+#include "OFstream.H"
+#include "rhoThermo.H"
+#include "EulerDdtScheme.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    //#include "createSponge.H"
+    #include "readEnvironmentalProperties.H"
+    #include "readThermo.H"
+    #include "createFields.H"
+    //#include "initContinuityErrs.H"
+    #include "initEnergy.H"
+    #include "energy.H"
+    
+    const Switch SIgravityWaves(mesh.schemes().lookup("SIgravityWaves"));
+    const Switch implicitU(mesh.schemes().lookup("implicitU"));
+    const Switch implicitT(mesh.schemes().lookup("implicitT"));
+    const Switch hydrostatic(mesh.schemes().lookup("hydrostatic"));
+
+    const dictionary& itsDict = mesh.solution().subDict("iterations");
+    const int nOuterCorr = itsDict.lookupOrDefault<int>("nOuterCorrectors", 2);
+    const int nCorr = itsDict.lookupOrDefault<int>("nCorrectors", 1);
+    const int nNonOrthCorr =
+        itsDict.lookupOrDefault<int>("nNonOrthogonalCorrectors", 0);
+
+    const scalar ocCoeff
+    (
+        readScalar(mesh.schemes().subDict("ddtSchemes").lookup("ocCoeff"))
+    );
+    const scalar ocAlpha = 1/(1+ocCoeff);
+
+    // Pre-defined time stepping scheme
+    fv::EulerDdtScheme<scalar> EulerDdt(mesh);
+    
+    turbulence->validate();   //- Validate turbulence fields after construction
+                            //  and update derived fields as required
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "compressibleCourantNo.H"
+
+        for (int ucorr=0; ucorr<nOuterCorr; ucorr++)
+        {
+            #include "rhoThetaEqn.H"
+            #include "UEqn.H"
+            // Exner and momentum equations
+            #include "exnerEqn.H"
+        }
+        #include "rhoThetaEqn.H"
+        //#include "compressibleContinuityErrs.H"
+
+        #include "thermoUpdate.H"
+        #include "energy.H"
+        //- Solve the turbulence equations and correct the turbulence viscosity
+        turbulence->correct();
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
--- src/prescribedFields/tracerField/sinusoidalTracerField.C
+++ src/prescribedFields/tracerField/sinusoidalTracerField.C
@@ -12,13 +12,13 @@ sinusoidalTracerField::sinusoidalTracerField
 :
     tracerField(velocityField),
     
-    a_x(dict.lookupOrDefault<scalar>("a_x", scalar(0))),
-    a_y(dict.lookupOrDefault<scalar>("a_y", scalar(0))),
-    a_z(dict.lookupOrDefault<scalar>("a_z", scalar(0))),
+    a_x(dict.lookupOrDefault<scalar>("a_x", scalar(1))),
+    a_y(dict.lookupOrDefault<scalar>("a_y", scalar(1))),
+    a_z(dict.lookupOrDefault<scalar>("a_z", scalar(1))),
     
-    b_x(dict.lookupOrDefault<scalar>("b_x", scalar(1))),
-    b_y(dict.lookupOrDefault<scalar>("b_y", scalar(1))),
-    b_z(dict.lookupOrDefault<scalar>("b_z", scalar(1))),
+    b_x(dict.lookupOrDefault<scalar>("b_x", scalar(0))),
+    b_y(dict.lookupOrDefault<scalar>("b_y", scalar(0))),
+    b_z(dict.lookupOrDefault<scalar>("b_z", scalar(0))),
     
     k_x(dict.lookupOrDefault<scalar>("k_x", scalar(0))),
     k_y(dict.lookupOrDefault<scalar>("k_y", scalar(0))),
@@ -31,6 +31,13 @@ sinusoidalTracerField::sinusoidalTracerField
     phi_x(dict.lookupOrDefault<scalar>("phi_x", scalar(0))),
     phi_y(dict.lookupOrDefault<scalar>("phi_y", scalar(0))),
     phi_z(dict.lookupOrDefault<scalar>("phi_z", scalar(0))),
+    
+    xmin(dict.lookupOrDefault<scalar>("xmin", -GREAT)),
+    xmax(dict.lookupOrDefault<scalar>("xmax", GREAT)),
+    ymin(dict.lookupOrDefault<scalar>("ymin", -GREAT)),
+    ymax(dict.lookupOrDefault<scalar>("ymax", GREAT)),
+    zmin(dict.lookupOrDefault<scalar>("zmin", -GREAT)),
+    zmax(dict.lookupOrDefault<scalar>("zmax", GREAT)),
     
     funcMax(dict.lookupOrDefault<scalar>("funcMax", GREAT)),
     funcMin(dict.lookupOrDefault<scalar>("funcMin", -GREAT))
@@ -45,9 +52,14 @@ scalar sinusoidalTracerField::tracerAt
     scalar x = p.x();
     scalar y = p.y();
     scalar z = p.z();
-    scalar func =  (a_x + b_x*Foam::cos(2*M_PI*k_x*(x-phi_x)/L_x))
-                 * (a_y + b_y*Foam::cos(2*M_PI*k_y*(y-phi_y)/L_y))
-                 * (a_z + b_z*Foam::cos(2*M_PI*k_z*(z-phi_z)/L_z));
+    scalar func =  0;
+    
+    if (x >= xmin && x <= xmax && y >= ymin && y <= ymax && z >= zmin && z <= zmax)
+    {
+        func = (a_x + b_x*Foam::cos(2*M_PI*k_x*(x-phi_x)/L_x))
+             * (a_y + b_y*Foam::cos(2*M_PI*k_y*(y-phi_y)/L_y))
+             * (a_z + b_z*Foam::cos(2*M_PI*k_z*(z-phi_z)/L_z));
+    }
     return max(min(func, funcMax), funcMin);
 }
 
